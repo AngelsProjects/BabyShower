020bfcb1fa2ad13e677c919c8c3356db525b5e50
Angel Arciniega <choy_1_10@hotmail.es>
2017-11-21 18:09:36 -0800

    done

---
+++ src/babyshower/BabyShower.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package babyshower;
+
+/**
+ *
+ * @author arcinima
+ */
+public class BabyShower {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        View view = new View();
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+    }
+    
+}
